77abf6beac60225c44ffeb2e4c48988f3e6db6ea
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-03-24 12:08:48 +0100

    Initial commit: resources are the web and the session track

---
+++ src/main/java/de/webis/cikm20_duplicates/util/SourceDocuments.java
@@ -0,0 +1,115 @@
+package de.webis.cikm20_duplicates.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.webis.trec_ndd.trec_collections.SharedTask;
+import de.webis.trec_ndd.trec_collections.CollectionConfiguration.TrecCollections;
+import de.webis.trec_ndd.trec_collections.SharedTask.TrecSharedTask;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.UtilityClass;
+import lombok.experimental.Wither;
+
+@UtilityClass
+public final class SourceDocuments {
+	private static final List<TrecSharedTask> TRACKS = Arrays.asList(
+			TrecSharedTask.WEB_2009, TrecSharedTask.WEB_2010,
+			TrecSharedTask.WEB_2011, TrecSharedTask.WEB_2012,
+
+			TrecSharedTask.WEB_2013, TrecSharedTask.WEB_2014,
+
+			TrecSharedTask.SESSION_2010, TrecSharedTask.SESSION_2011,
+			TrecSharedTask.SESSION_2012,
+
+			TrecSharedTask.SESSION_2013, TrecSharedTask.SESSION_2014
+	);
+	
+	public static final List<SourceDocument> ALL_DOCS_FOR_WHICH_DUPLICATES_SHOULD_BE_SEARCHED = getAllDocumentsForWhichDuplicatesShouldBeSearched();
+	
+	@Data
+	@Wither
+	@AllArgsConstructor
+	public static class SourceDocument {
+		private final TrecSharedTask task;
+		private final TrecCollections collection;
+		private final String documentId;
+		private final String topic;
+		
+		public String toString() {
+			return collection.name() + "::" + task.name() + "::" + topic + "::" + documentId;
+		}
+	}
+
+	private static List<SourceDocument> getAllDocumentsForWhichDuplicatesShouldBeSearched() {
+		List<SourceDocument> ret = new LinkedList<>();
+		for (SharedTask task : TRACKS) {
+			Set<String> topics = task.documentJudgments().getData().keySet();
+
+			for (String topic : topics) {
+				Set<String> judgedDocs = task.documentJudgments().getData().get(topic).keySet();
+				for (String doc : judgedDocs) {
+					ret.add(new SourceDocument(
+							(TrecSharedTask) task, collectionForTask(task),
+							doc, topic
+					));
+				}
+			}
+		}
+
+		Collections.sort(ret, (a,b) -> a.toString().compareTo(b.toString()));
+		
+		return Collections.unmodifiableList(ret);
+	}
+	
+	public static Map<TrecSharedTask, List<SourceDocument>> taskToDocuments() {
+		Map<TrecSharedTask, List<SourceDocument>> ret = new LinkedHashMap<>();
+		
+		for(SourceDocument doc : ALL_DOCS_FOR_WHICH_DUPLICATES_SHOULD_BE_SEARCHED) {
+			if(!ret.containsKey(doc.getTask())) {
+				ret.put(doc.getTask(), new LinkedList<>());
+			}
+			
+			ret.get(doc.getTask()).add(doc);
+		}
+		
+		return ret;
+	}
+	
+	public static Map<String, Set<String>> topicsToDocumentIds() {
+		Map<String, Set<String>> ret = new LinkedHashMap<>();
+		
+		for(SourceDocument doc : ALL_DOCS_FOR_WHICH_DUPLICATES_SHOULD_BE_SEARCHED) {
+			String topic = doc.collection.name() + "::" + doc.task.name() + "::" + doc.topic;
+			if(!ret.containsKey(topic)) {
+				ret.put(topic, new HashSet<>());
+			}
+			
+			ret.get(topic).add(doc.documentId);
+		}
+		
+		return ret;
+	}
+	
+	private static TrecCollections collectionForTask(SharedTask task) {
+		if(Arrays.asList(TrecSharedTask.WEB_2009, TrecSharedTask.WEB_2010,
+			TrecSharedTask.WEB_2011, TrecSharedTask.WEB_2012).contains(task)) {
+			return TrecCollections.CLUEWEB09;
+		} else if(Arrays.asList(TrecSharedTask.WEB_2013, TrecSharedTask.WEB_2014).contains(task)) {
+			return TrecCollections.CLUEWEB12;
+		} else if(Arrays.asList(TrecSharedTask.SESSION_2010, TrecSharedTask.SESSION_2011,
+				TrecSharedTask.SESSION_2012).contains(task)) {
+			return TrecCollections.CLUEWEB09;
+		} else if(Arrays.asList(TrecSharedTask.SESSION_2013, TrecSharedTask.SESSION_2014).contains(task)) {
+			return TrecCollections.CLUEWEB12;
+		}
+		
+		throw new RuntimeException("fixme");
+	}
+}
---
+++ src/test/java/de/webis/cikm20_duplicates/util/SourceDocumentsTest.java
@@ -0,0 +1,69 @@
+package de.webis.cikm20_duplicates.util;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.approvaltests.Approvals;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.webis.cikm20_duplicates.util.SourceDocuments.SourceDocument;
+
+/**
+ * 
+ * @author Maik Fröbe
+ *
+ */
+public class SourceDocumentsTest {
+	@Test
+	public void approveSourceDocumentsOfAllTasks() {
+		List<SourceDocument> actual = SourceDocuments.ALL_DOCS_FOR_WHICH_DUPLICATES_SHOULD_BE_SEARCHED;
+		
+		Approvals.verifyAsJson(actual.stream().map(i -> i.toString()).collect(Collectors.toList()));
+	}
+	
+	@Test
+	public void approveCountOfDocsToTransfer() {
+		int expected = 203265;
+		int actual = SourceDocuments.ALL_DOCS_FOR_WHICH_DUPLICATES_SHOULD_BE_SEARCHED.size();
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void approveTaskCount() {
+		int expected = 11;
+		int actual = SourceDocuments.taskToDocuments().keySet().size();
+
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void approveTopicCount() {
+		int expected = 644;
+		int actual = SourceDocuments.topicsToDocumentIds().keySet().size();
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void approveDocCountPerTopic() {
+		Map<String, Set<String>> actual = SourceDocuments.topicsToDocumentIds();
+		Map<String, Integer> toApprove = new LinkedHashMap<>(); 
+		actual.entrySet().forEach(i -> toApprove.put(i.getKey(), i.getValue().size()));
+		
+		Approvals.verifyAsJson(toApprove);
+	}
+	
+	@Test
+	public void approveDocCountPerTask() {
+		Map<String, Integer> toApprove = new LinkedHashMap<>(); 
+		SourceDocuments.taskToDocuments().entrySet().forEach(i -> toApprove.put(i.getKey().name(), i.getValue().size()));
+		
+
+		Approvals.verifyAsJson(toApprove);
+	}
+}
