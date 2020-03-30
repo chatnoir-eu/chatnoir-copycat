36e2b73272ebd13803285ee1fcaafea6047a56a3
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-03-30 22:39:33 +0200

    Add similarity

--- src/main/java/de/webis/cikm20_duplicates/util/FingerPrintUtil.java
+++ src/main/java/de/webis/cikm20_duplicates/util/FingerPrintUtil.java
@@ -1,7 +1,6 @@
 package de.webis.cikm20_duplicates.util;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,6 +9,7 @@ import java.util.stream.IntStream;
 import de.aitools.ir.fingerprinting.hashfunction.SimHash;
 import de.aitools.ir.fingerprinting.representation.HashVector;
 import de.aitools.ir.fingerprinting.representation.HashVectorSha3;
+import de.aitools.ir.fingerprinting.representer.Hash;
 import de.webis.trec_ndd.trec_collections.CollectionDocument;
 import de.webis.trec_ndd.util.NGramms;
 import info.debatty.java.lsh.MinHash;
@@ -58,31 +58,17 @@ public class FingerPrintUtil {
 			public List<Integer> fingerprint(CollectionDocument doc) {
 				byte[] hash = hashVector(doc, bitsInSimHash);
 				
-				return hashToIntegers(hash, k);
+				return HashTransformationUtil.hashToIntegers(hash, k);
 			}
 
 			@Override
 			public double similarity(List<Integer> a, List<Integer> b) {
-				return 1.0;
+				byte[] aArr = HashTransformationUtil.integersToHash(a);
+				byte[] bArr = HashTransformationUtil.integersToHash(b);
+				
+				return 1.0 - (((double)Hash.getHammingDistance(aArr, bArr))/(double)aArr.length);
 			}
 		};
-	}
-	
-	static List<Integer> hashToIntegers(byte[] hash, int k) {
-		if(k != 3 || hash == null || hash.length != 8) {
-			throw new IllegalArgumentException("");
-		}
-		
-		return Arrays.asList(
-			// 0-15. Use 0 bytes at positions 3 and 4 to identify bytes 0-15.
-			HashTransformationUtil.bytesToInt(new byte[] {hash[0], hash[1], 0x0, 0x0}),
-			// 16-31. Use 0 bytes at positions 1 and 2 to identify bytes 16-31.
-			HashTransformationUtil.bytesToInt(new byte[] {0x0, 0x0, hash[2], hash[3]}),
-			// 32-47. Use 0 bytes at positions 2 and 4 to identify bytes 32-47.
-			HashTransformationUtil.bytesToInt(new byte[] {0x0, hash[4], 0x0, hash[5]}),
-			// 48-63. Use 0 bytes at positions 1 and 4 to identify bytes 48-63.
-			HashTransformationUtil.bytesToInt(new byte[] {0x0, hash[6], hash[7], 0x0})
-		);
 	}
 	
 	static byte[] hashVector(CollectionDocument doc, int bitsInSimHash) {
--- src/test/java/de/webis/cikm20_duplicates/util/HashToIntTransformationTest.java
+++ src/test/java/de/webis/cikm20_duplicates/util/HashToIntTransformationTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import static de.webis.cikm20_duplicates.util.HashTransformationUtil.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HashToIntTransformationTest {
 
 	@Test(expected = IllegalArgumentException.class)
@@ -35,8 +38,44 @@ public class HashToIntTransformationTest {
 		intToBytes(0,3);
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForTooSmallK() {
+		hashToIntegers(new byte[] {0,0,0,0,0,0,0,0}, 2);
+	}
+	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForTooLargeK() {
+		hashToIntegers(new byte[] {0,0,0,0,0,0,0,0}, 4);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForTooSmallArray() {
+		hashToIntegers(new byte[] {0,0,0,0,0,0,0}, 3);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForTooLargeArray() {
+		hashToIntegers(new byte[] {0,0,0,0,0,0,0,0,0}, 3);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForNullInts() {
+		integersToHash(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForTooFewInts() {
+		integersToHash(Arrays.asList(0,0,0));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void checkExceptionIsThrownForTooMuchInts() {
+		integersToHash(Arrays.asList(0,0,0,0,0));
+	}
+	
 	@Test
-	public void approveHashWithAllZerosAndLength64() {
+	public void approveHashWithAllZerosAndLength32() {
 		byte[] bytes = new byte[] {0x0, 0x0, 0x0, 0x0};
 
 		int expected = 0;
@@ -50,7 +89,7 @@ public class HashToIntTransformationTest {
 	}
 	
 	@Test
-	public void approveHashWithFourOnesAndLength64() {
+	public void approveHashWithFourOnesAndLength32() {
 		byte[] bytes = new byte[] {0x1, 0x1, 0x1, 0x1};
 
 		int expected = 16843009;
@@ -64,7 +103,7 @@ public class HashToIntTransformationTest {
 	}
 
 	@Test
-	public void approveHashWithFourDifferentBytesAndLength64() {
+	public void approveHashWithFourDifferentBytesAndLength32() {
 		byte[] bytes = new byte[] {0x1, 0x2, 0x3, 0x4};
 
 		int expected = 16909060;
@@ -78,7 +117,7 @@ public class HashToIntTransformationTest {
 	}
 	
 	@Test
-	public void approveHashWithLargestBytesAndLength64() {
+	public void approveHashWithLargestBytesAndLength32() {
 		byte[] bytes = new byte[] {0x7f, 0x7f, 0x7f, 0x7f};
 
 		int expected = 2139062143;
@@ -92,7 +131,7 @@ public class HashToIntTransformationTest {
 	}
 	
 	@Test
-	public void approveHashWithSmallestBytesAndLength64() {
+	public void approveHashWithSmallestBytesAndLength32() {
 		byte[] bytes = new byte[] {-0x80, -0x80, -0x80, -0x80};
 
 		int expected = (int) (-2147483648 + 8421504);
@@ -106,7 +145,7 @@ public class HashToIntTransformationTest {
 	}
 	
 	@Test
-	public void approveHashWithOnlyMinusOneBytesAndLength64() {
+	public void approveHashWithOnlyMinusOneBytesAndLength32() {
 		byte[] bytes = new byte[] {-0x01, -0x01, -0x01, -0x01};
 
 		int expected = -1;
@@ -120,7 +159,7 @@ public class HashToIntTransformationTest {
 	}
 	
 	@Test
-	public void approveHashWithSomeMinusOneBytesAndLength64() {
+	public void approveHashWithSomeMinusOneBytesAndLength32() {
 		byte[] bytes = new byte[] {-0x01, 0x0, 0x01, -0x01};
 
 		int expected = (int) (-2147483648 + 2130706943);
@@ -134,7 +173,7 @@ public class HashToIntTransformationTest {
 	}
 	
 	@Test
-	public void approveHashWithOneMinusAndSomePlusBytesAndLength64() {
+	public void approveHashWithOneMinusAndSomePlusBytesAndLength32() {
 		byte[] bytes = new byte[] {0x1, 0x2, 0x3, -0x01};
 
 		int expected = 16909311;
@@ -145,5 +184,76 @@ public class HashToIntTransformationTest {
 		Assert.assertEquals(expected, actual);
 		Assert.assertEquals(expectedBinary, actualBinary);
 		Assert.assertArrayEquals(bytes, intToBytes(actual, 4));
+	}
+	
+	@Test
+	public void approveTransformationForAllZeroBytesAndLength64() {
+		byte[] bytes = new byte[] {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+		List<Integer> ints = Arrays.asList(0, 0, 0, 0);
+		
+		Assert.assertEquals(ints, hashToIntegers(bytes, 3));
+		Assert.assertArrayEquals(bytes, integersToHash(ints));
+	}
+	
+	@Test
+	public void approveTransformationForAscendingBytesAndLength64() {
+		byte[] bytes = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+		List<Integer> ints = Arrays.asList(16908288, 772, 327686, 460800);
+		
+		Assert.assertEquals(ints, hashToIntegers(bytes, 3));
+		Assert.assertArrayEquals(bytes, integersToHash(ints));
+	}
+	
+	@Test
+	public void approveTransformationForDescendingBytesAndLength64() {
+		byte[] bytes = new byte[] {0x08, 0x07, 0x06, 0x05, 0x04, 0x04, 0x02, 0x01};
+		List<Integer> ints = Arrays.asList(134676480, 1541, 262148, 131328);
+		
+		Assert.assertEquals(ints, hashToIntegers(bytes, 3));
+		Assert.assertArrayEquals(bytes, integersToHash(ints));
+	}
+	
+	@Test
+	public void approveTransformationForAllMinusOneAndLength64() {
+		byte[] bytes = new byte[] {-0x01, -0x01, -0x01, -0x01, -0x01, -0x01, -0x01, -0x01};
+		List<Integer> ints = Arrays.asList(-65536, 65535, 16711935, 16776960);
+		
+		Assert.assertEquals(ints, hashToIntegers(bytes, 3));
+		Assert.assertArrayEquals(bytes, integersToHash(ints));
+	}
+	
+	@Test
+	public void approveTransformationForAlternatingMinusOneMaxAndLength64() {
+		byte[] bytes = new byte[] {-0x01, 0x7f, -0x01, 0x7f, -0x01, 0x7f, -0x01, 0x7f};
+		List<Integer> ints = Arrays.asList(-8454144, 65407, 16711807, 16744192);
+		
+		Assert.assertEquals(ints, hashToIntegers(bytes, 3));
+		Assert.assertArrayEquals(bytes, integersToHash(ints));
+	}
+	
+	@Test
+	public void approveTransformationForAlternatingMaxAndMinusOneAndLength64() {
+		byte[] bytes = new byte[] {0x7f, -0x01, 0x7f, -0x01, 0x7f, -0x01, 0x7f, -0x01};
+		List<Integer> ints = Arrays.asList(2147418112, 32767, 8323327, 8388352);
+		
+		Assert.assertEquals(ints, hashToIntegers(bytes, 3));
+		Assert.assertArrayEquals(bytes, integersToHash(ints));
+	}
+	
+	@Test
+	public void approveWithRandomIntsFor64Bits() {
+		for(int i=0; i<1000; i++) {
+			byte[] bytes = new byte[] {rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand()};
+			String message = "[" + ((int) bytes[0]) + "," + ((int) bytes[1]) + "," + ((int) bytes[2]) + ","
+							+((int) bytes[3]) + "," +((int) bytes[4]) + "," + ((int) bytes[5]) + ","
+							+((int) bytes[6]) + "," +((int) bytes[7]) + "]";
+		
+			Assert.assertArrayEquals("Look at " + message, bytes, integersToHash(hashToIntegers(bytes, 3)));
+			Assert.assertEquals("Look at " + message, hashToIntegers(bytes, 3), hashToIntegers(integersToHash(hashToIntegers(bytes, 3)), 3));
+		}
+	}
+	
+	private byte rand() {
+		return (byte) (int)((Math.random()*255) -128);
 	}
 }
