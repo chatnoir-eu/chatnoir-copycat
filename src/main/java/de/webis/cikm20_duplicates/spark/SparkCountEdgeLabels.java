515da79880cf389f16bbd10a3d8996cb15edb0a8
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-04-19 10:13:22 +0200

    count edges

---
+++ src/main/java/de/webis/cikm20_duplicates/spark/SparkCountEdgeLabels.java
@@ -0,0 +1,84 @@
+package de.webis.cikm20_duplicates.spark;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.math.BigIntegerMath;
+
+import lombok.SneakyThrows;
+import scala.Tuple2;
+
+public class SparkCountEdgeLabels {
+
+	public static JavaRDD<String> countEdgeLabels(JavaRDD<String> exactDuplicates, JavaRDD<String> pairs) {
+		JavaPairRDD<String, BigInteger> a = exactDuplicates.flatMapToPair(i -> flatten(i).iterator())
+				.reduceByKey((i,j) -> i.add(j), 10)
+				.filter(i -> !i._2.equals(BigInteger.ZERO));
+		
+		JavaPairRDD<String, BigInteger> b = pairs.mapToPair(i -> toTuple(i))
+				.reduceByKey((i,j) -> i.add(j), 10)
+				.filter(i -> !i._2.equals(BigInteger.ZERO));
+		
+		return a.union(b)
+				.reduceByKey((i,j) -> i.add(j), 10)
+				.filter(i -> !i._2.equals(BigInteger.ZERO))
+				.map(i -> "{\"" + i._1() + "\":" + i._2() + "}");
+	}
+
+	private static Tuple2<String, BigInteger> toTuple(String a) {
+		String[] parsed = a.split(";");
+		if(parsed.length != 3) {
+			throw new RuntimeException("tbd");
+		}
+		
+		String name;
+		
+		if((parsed[0].startsWith("clueweb09") && parsed[1].startsWith("clueweb12")) || (parsed[0].startsWith("clueweb12") && parsed[1].startsWith("clueweb09"))) {
+			name = "clueweb09<->clueweb12";
+		} else if (parsed[0].startsWith("clueweb09") && parsed[1].startsWith("clueweb09")) {
+			name = "clueweb09<->clueweb09";
+		} else if (parsed[0].startsWith("clueweb12") && parsed[1].startsWith("clueweb12")) {
+			name = "clueweb12<->clueweb12";
+		} else {
+			throw new RuntimeException("Can not handle '" + a + "'.");
+		}
+		
+		return new Tuple2<>(name +" (k=" + Integer.parseInt(parsed[2]) + ")", BigInteger.ONE);
+	}
+	
+	@SneakyThrows
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private static List<Tuple2<String, BigInteger>> flatten(String src) {
+		Map<String, Object> s = new ObjectMapper().readValue(src, Map.class);
+		List<String> docs = (List) s.get("equivalentDocuments");
+		
+		BigInteger all = edgesBetweenFullyConnectedGraph(docs.size());
+		BigInteger clueWeb09 = edgesBetweenFullyConnectedGraph(docs.stream().filter(i -> i.startsWith("clueweb09")).count());
+		BigInteger clueWeb12 = edgesBetweenFullyConnectedGraph(docs.stream().filter(i -> i.startsWith("clueweb12")).count());
+		
+		return Arrays.asList(
+			new Tuple2<>("clueweb09<->clueweb09 (k=0)", clueWeb09),
+			new Tuple2<>("clueweb12<->clueweb12 (k=0)", clueWeb12),
+			new Tuple2<>("clueweb09<->clueweb12 (k=0)", all.subtract(clueWeb12).subtract(clueWeb09))
+		);
+	}
+	
+	private static BigInteger edgesBetweenFullyConnectedGraph(long count) {
+		if(count <= 1) {
+			return BigInteger.ZERO;
+		}
+		
+		BigInteger nodeCount = BigIntegerMath.factorial((int) count);
+		BigInteger denominator = BigInteger.valueOf(2l).multiply(BigIntegerMath.factorial(((int)count) -2));
+		
+		return nodeCount.divide(denominator);
+	}
+	
+	
+}
---
+++ src/test/java/de/webis/cikm20_duplicates/spark/SparkCountEdgeLabelsIntegrationTest.java
@@ -0,0 +1,79 @@
+package de.webis.cikm20_duplicates.spark;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.approvaltests.Approvals;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.holdenkarau.spark.testing.SharedJavaSparkContext;
+
+public class SparkCountEdgeLabelsIntegrationTest extends SharedJavaSparkContext {
+
+	@Test
+	public void testWithEmptyExactDuplicatesAndPairs() {
+		List<String> expected = Collections.emptyList();
+		List<String> actual = countAllEdges(rdd(), rdd());
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testWithSomeClueWeb09DuplicatesAndEmptyPairs() {
+		List<String> actual = countAllEdges(rdd(
+				"{\"equivalentDocuments\": [\"clueweb09-1\",\"clueweb09-3\"],\"hash\":[]}",
+				"{\"equivalentDocuments\": [\"clueweb09-2\",\"clueweb09-4\",\"clueweb09-5\",\"clueweb09-6\"],\"hash\":[]}"
+		), rdd());
+		
+		//1 + 6 = 7
+		Approvals.verify(actual);
+	}
+	
+	@Test
+	public void testWithSomeClueWeb12DuplicatesAndEmptyPairs() {
+		List<String> actual = countAllEdges(rdd(
+				"{\"equivalentDocuments\": [\"clueweb12-1\",\"clueweb12-3\",\"clueweb12-3\"],\"hash\":[]}"
+		), rdd());
+		
+		//3
+		Approvals.verify(actual);
+	}
+	
+	@Test
+	public void testWithSomeClueWeb09And12DuplicatesAndEmptyPairs() {
+		List<String> actual = countAllEdges(rdd(
+				"{\"equivalentDocuments\": [\"clueweb09-2\",\"clueweb12-4\",\"clueweb09-5\",\"clueweb09-6\"],\"hash\":[]}"
+		), rdd());
+		
+		//3 ; 3 ; 0
+		Approvals.verify(actual);
+	}
+	
+	@Test
+	public void testWithSomeClueWeb09And12DuplicatesAndSomePairs() {
+		List<String> actual = countAllEdges(rdd(
+				"{\"equivalentDocuments\": [\"clueweb09-2\",\"clueweb12-4\",\"clueweb09-5\",\"clueweb09-6\"],\"hash\":[]}"
+		), rdd(
+				"clueweb09-2;clueweb09-3;2",
+				"clueweb09-2;clueweb09-3;2",
+				"clueweb09-2;clueweb09-3;1",
+				"clueweb12-2;clueweb09-3;3",
+				"clueweb09-2;clueweb12-3;3",
+				"clueweb12-2;clueweb12-3;1"
+		));
+		
+		Approvals.verify(actual);
+	}
+	
+	private List<String> countAllEdges(JavaRDD<String> exactDuplicates, JavaRDD<String> pairs) {
+		JavaRDD<String> ret = SparkCountEdgeLabels.countEdgeLabels(exactDuplicates, pairs);
+		return SparkCreateSourceDocumentsIntegrationTest.sorted(ret);
+	}
+	
+	private JavaRDD<String> rdd(String...vals) {
+		return jsc().parallelize(Arrays.asList(vals));
+	}
+}
