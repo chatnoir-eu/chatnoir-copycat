2393749cf283d33e55ba582542d320d8dd7cd172
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-05-28 05:36:24 +0200

    deduplicate cw09-12

--- src/main/java/de/webis/cikm20_duplicates/spark/SparkDeduplicateCandidates.java
+++ src/main/java/de/webis/cikm20_duplicates/spark/SparkDeduplicateCandidates.java
@@ -15,27 +15,28 @@ import scala.Tuple2;
  *
  */
 public class SparkDeduplicateCandidates {
-//	public static void main(String[] args) {
-//		try (JavaSparkContext context = context()) {
-//			context.textFile("cikm2020/near-duplicate-tasks-cw09-cw12")
-//				.map(i-> DeduplicationTask.fromString(i))
-//				.flatMap(i -> ClientLocalDeduplication.fullDeduplication(i.getEntries()).iterator())
-//				.saveAsTextFile("cikm2020/deduplication/near-duplicates/cw09-cw12");
-//		}
-//	}
-	
 	public static void main(String[] args) {
 		String corpus = "cw09-cw12";
 		try (JavaSparkContext context = context()) {
-			JavaPairRDD<Integer, Integer> deduplicationTaskSizeToCount = context.textFile(inputPath(corpus))
-				.mapToPair(i-> new Tuple2<>(DeduplicationTask.fromString(i).getEntries().size(), 1))
-				.reduceByKey((a,b) -> a+b);
-			
-			deduplicationTaskSizeToCount
-				.map(i -> "{\"groupSize\":" + i._1() + ",\"count\":" + i._2() + "}")
-				.saveAsTextFile("cikm2020/document-fingerprints-final/deduplication-task-size-to-count/" + corpus);
+			context.textFile(inputPath(corpus))
+				.map(i-> DeduplicationTask.fromString(i))
+				.flatMap(i -> ClientLocalDeduplication.fullDeduplication(i.getEntries()).iterator())
+				.saveAsTextFile("cikm2020/deduplication-final/64BitK3SimHashThreeAndFiveGramms/" + corpus + "-near-duplicates-without-exact-duplicates");
 		}
 	}
+	
+//	public static void main(String[] args) {
+//		String corpus = "cw09-cw12";
+//		try (JavaSparkContext context = context()) {
+//			JavaPairRDD<Integer, Integer> deduplicationTaskSizeToCount = context.textFile(inputPath(corpus))
+//				.mapToPair(i-> new Tuple2<>(DeduplicationTask.fromString(i).getEntries().size(), 1))
+//				.reduceByKey((a,b) -> a+b);
+//			
+//			deduplicationTaskSizeToCount
+//				.map(i -> "{\"groupSize\":" + i._1() + ",\"count\":" + i._2() + "}")
+//				.saveAsTextFile("cikm2020/document-fingerprints-final/deduplication-task-size-to-count/" + corpus);
+//		}
+//	}
 	
 	private static String inputPath(String corpus) {
 		return "cikm2020/deduplication-final/64BitK3SimHashThreeAndFiveGramms/" + corpus +"-near-duplicate-tasks";
