7cf91937c63b9979523ac734e9541f62e9ffd894
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-09-03 08:47:31 +0200

    Add version 0 of first pipeline step

--- src/main/java/de/webis/cikm20_duplicates/app/CreateDocumentRepresentations.java
+++ src/main/java/de/webis/cikm20_duplicates/app/CreateDocumentRepresentations.java
@@ -27,49 +27,45 @@ import net.sourceforge.argparse4j.inf.Namespace;
 import scala.Tuple2;
 
 public class CreateDocumentRepresentations {
-	
+
 	public static void main(String[] args) {
 		Namespace parsedArgs = validArgumentsOrNull(args);
-		
-		if(parsedArgs == null) {
+
+		if (parsedArgs == null) {
 			return;
 		}
-		
-		try(JavaSparkContext context = context()) {
+
+		try (JavaSparkContext context = context()) {
 			JavaPairRDD<LongWritable, WarcRecord> records = WARCParsingUtil.records(context, parsedArgs);
 			JavaRDD<CollectionDocument> parsedDocuments = parsedDocuments(records);
-			
-			JavaRDD<DocumentWithFingerprint> fingerprints = SparkCreateSourceDocuments.fingerprintAllDocuments(null, parsedDocuments, SparkCreateSourceDocuments.PRODUCTION_FINGERPRINTS);
-			
-			fingerprints
-				.filter(i -> i != null)
-				.map(i -> i.toString())
-				.saveAsTextFile(parsedArgs.getString(ArgumentParsingUtil.ARG_OUTPUT), BZip2Codec.class);
+			if (parsedDocuments.getNumPartitions() < 100) {
+				parsedDocuments = parsedDocuments.repartition(1000);
+			}
+
+			JavaRDD<DocumentWithFingerprint> fingerprints = SparkCreateSourceDocuments.fingerprintAllDocuments(null,
+					parsedDocuments, SparkCreateSourceDocuments.PRODUCTION_FINGERPRINTS);
+
+			fingerprints.filter(i -> i != null).map(i -> i.toString())
+					.saveAsTextFile(parsedArgs.getString(ArgumentParsingUtil.ARG_OUTPUT), BZip2Codec.class);
 		}
 	}
-	
+
 	private static JavaRDD<CollectionDocument> parsedDocuments(JavaPairRDD<LongWritable, WarcRecord> records) {
-		if(records.getNumPartitions() < 100) {
-			JavaRDD<Tuple2<Map<String, String>, String>> parsedRecords = records.map(i -> transformToCollectionDocument(i._2()));
-			parsedRecords = parsedRecords.filter(i -> i!= null).repartition(1000).cache();
-			return parsedRecords.map(i -> transformToCollectionDocument(i._1(), i._2())).filter(i -> i != null);
-		} else {
-			return records.map(i -> {
-				Tuple2<Map<String, String>, String> r = transformToCollectionDocument(i._2());
-				return transformToCollectionDocument(r._1(), r._2());
-			}).filter(i -> i != null);
-		}
+		return records.map(i -> {
+			Tuple2<Map<String, String>, String> r = transformToCollectionDocument(i._2());
+			return r == null ? null : transformToCollectionDocument(r._1(), r._2());
+		}).filter(i -> i != null);
 	}
-	
+
 	@SneakyThrows
 	public static Tuple2<Map<String, String>, String> transformToCollectionDocument(WarcRecord record) {
 		if (record == null) {
 			return null;
 		}
-		
+
 		Map<String, String> header = lowercasedHeaders(record);
 		String contentBody = record.getContent();
-		
+
 		if (contentBody.getBytes().length > 1024 * 1024) {
 			// ignore large files
 			return null;
@@ -77,7 +73,7 @@ public class CreateDocumentRepresentations {
 
 		return new Tuple2<>(header, contentBody);
 	}
-	
+
 	public static CollectionDocument transformToCollectionDocument(Map<String, String> header, String contentBody) {
 		try {
 			return transformToCollectionDocument(header, Jsoup.parse(contentBody));
@@ -85,21 +81,21 @@ public class CreateDocumentRepresentations {
 			return null;
 		}
 	}
-	
+
 	@SneakyThrows
 	private static CollectionDocument transformToCollectionDocument(Map<String, String> header, Document doc) {
 		String id = header.get("warc-trec-id");
-		if(id == null || id.isEmpty()) {
+		if (id == null || id.isEmpty()) {
 			id = header.get("warc-record-id");
 		}
-		
+
 		String targetUri = header.get("warc-target-uri");
-		
+
 		CollectionDocument ret = CollectionDocument.collectionDocument(doc.text(), id);
 		ret.setUrl(new URL(targetUri));
 		ret.setCanonicalUrl(SparkCanonicalLinkGraphExtraction.extractCanonicalLinkOrNull(targetUri, doc));
 		ret.setCrawlingTimestamp(header.get("warc-date"));
-		
+
 		return ret;
 	}
 
@@ -114,10 +110,10 @@ public class CreateDocumentRepresentations {
 
 		return new JavaSparkContext(conf);
 	}
-	
+
 	static Namespace validArgumentsOrNull(String[] args) {
 		ArgumentParser parser = argParser();
-		
+
 		try {
 			return parser.parseArgs(args);
 		} catch (ArgumentParserException e) {
@@ -125,24 +121,20 @@ public class CreateDocumentRepresentations {
 			return null;
 		}
 	}
-	
+
 	private static ArgumentParser argParser() {
 		ArgumentParser ret = ArgumentParsers.newFor(ArgumentParsingUtil.TOOL_NAME + ": CreateDocumentRepresentations")
-			.addHelp(Boolean.TRUE)
-			.build();
-		
-		ret.addArgument("-i", "--" + ArgumentParsingUtil.ARG_INPUT)
-			.required(Boolean.TRUE)
-			.help("The input path that is passed to JavaSparkContext.hadoopFile to extract Documents from warc files. E.g. 's3a://corpus-clueweb09'.");
-		
-		ret.addArgument("-o", "--" + ArgumentParsingUtil.ARG_OUTPUT)
-			.required(Boolean.TRUE)
-			.help("The resulting document representations are stored under this location.");
+				.addHelp(Boolean.TRUE).build();
 
-		ret.addArgument("-f", "--" + ArgumentParsingUtil.ARG_FORMAT)
-			.required(Boolean.TRUE)
-			.choices(ArgumentParsingUtil.InputFormats.allInputFormats());
-		
+		ret.addArgument("-i", "--" + ArgumentParsingUtil.ARG_INPUT).required(Boolean.TRUE).help(
+				"The input path that is passed to JavaSparkContext.hadoopFile to extract Documents from warc files. E.g. 's3a://corpus-clueweb09'.");
+
+		ret.addArgument("-o", "--" + ArgumentParsingUtil.ARG_OUTPUT).required(Boolean.TRUE)
+				.help("The resulting document representations are stored under this location.");
+
+		ret.addArgument("-f", "--" + ArgumentParsingUtil.ARG_FORMAT).required(Boolean.TRUE)
+				.choices(ArgumentParsingUtil.InputFormats.allInputFormats());
+
 		return ret;
 	}
 }
