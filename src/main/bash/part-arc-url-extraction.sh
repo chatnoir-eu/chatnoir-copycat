d9ce9fd1a99755e35e0849635859375ee03315ff
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-09-22 09:06:38 +0200

    mf

--- src/main/bash/part-arc-url-extraction.sh
+++ src/main/bash/part-arc-url-extraction.sh
@@ -3,6 +3,6 @@
 hdfs dfs -rm -r -f internet-archive/analysis-in-progress/cc-2008-urls/part-${1}
 
 ./src/main/bash/arc-url-extraction.sh \
-	--input s3a://corpus-commoncrawl-main-2008/*/*/*/*/*/*${1}_0.arc.gz \
+	--input s3a://corpus-commoncrawl-main-2008/*/*/*/*/*/*${1}_*.arc.gz \
 	--output internet-archive/analysis-in-progress/cc-2008-urls/part-${1}
 
