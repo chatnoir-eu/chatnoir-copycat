f53f06cf6c7e55cdb7d69c4684ed3b59f630e423
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-09-22 09:42:29 +0200

    mf

--- src/main/java/de/webis/cikm20_duplicates/app/ArcInputFormat.java
+++ src/main/java/de/webis/cikm20_duplicates/app/ArcInputFormat.java
@@ -2,6 +2,7 @@ package de.webis.cikm20_duplicates.app;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
@@ -19,6 +20,8 @@ import org.archive.io.ArchiveRecord;
 import org.archive.io.arc.ARCReader;
 import org.archive.io.arc.ARCReaderFactory;
 import org.archive.io.arc.ARCRecord;
+
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 
 import lombok.SneakyThrows;
 
@@ -83,7 +86,7 @@ public class ArcInputFormat extends FileInputFormat<LongWritable, ARCRecord> {
 		public void initialize(String path, InputStream is) {
 			key = null;
 			value = null;
-			iter = MyARCReaderFactory.get(new Path(path), is).iterator();
+			iter = MyARCReaderFactory.getIteratorOrEmptyIterator(new Path(path), is);
 		}
 
 		@Override
@@ -108,7 +111,7 @@ public class ArcInputFormat extends FileInputFormat<LongWritable, ARCRecord> {
 	
 				}
 			} catch (Exception e) {
-				//FIXME: Remove this: use proper handling
+//				FIXME: REMOVE THIS
 				return false;
 			}
 		}
@@ -118,8 +121,13 @@ public class ArcInputFormat extends FileInputFormat<LongWritable, ARCRecord> {
 		private static MyARCReaderFactory factory = new MyARCReaderFactory();
 
 		@SneakyThrows
-		public static ARCReader get(Path path, InputStream is) {
-			return (ARCReader) factory.getArchiveReader(path.toString(), is, Boolean.TRUE);
+		public static Iterator<ArchiveRecord> getIteratorOrEmptyIterator(Path path, InputStream is) {
+			try {
+				return ((ARCReader) factory.getArchiveReader(path.toString(), is, Boolean.TRUE)).iterator();
+			} catch (Exception e) {
+//				FIXME: REMOVE THIS
+				return Collections.emptyIterator();
+			}
 		}
 	}
 }
