fe9f98a298de559a60cd89f8aa4bda7451c77275
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-10-01 08:07:23 +0200

    improve parameters for near-duplicate-enrichment

--- src/main/bash/enrich-near-duplicates-with-s3-scores.sh
+++ src/main/bash/enrich-near-duplicates-with-s3-scores.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-PARALLELISM=500
 export HADOOP_CONF_DIR=/home/kibi9872/web-search-cikm2020-resource-paper-code/hadoop-conf
 export SPARK_HOME=/home/kibi9872/web-search-cikm2020-resource-paper-code/local-spark
 
@@ -9,11 +8,12 @@ export SPARK_HOME=/home/kibi9872/web-search-cikm2020-resource-paper-code/local-s
         --conf "spark.speculation.interval=5000ms" \
         --conf "spark.speculation.multiplier=5" \
         --conf "spark.speculation.quantile=0.90" \
-        --conf "spark.dynamicAllocation.maxExecutors=1500" \
+        --conf "spark.dynamicAllocation.maxExecutors=450" \
         --deploy-mode cluster \
         --class de.webis.cikm20_duplicates.app.EnrichSimHashNearDuplicatesWithS3Similarity \
-        --conf spark.default.parallelism=${PARALLELISM}\
-        --num-executors ${PARALLELISM}\
+        --conf spark.default.parallelism=50000\
+        --executor-cores 1\
+        --num-executors 500\
         --driver-memory 5G\
         target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar ${@}
 
