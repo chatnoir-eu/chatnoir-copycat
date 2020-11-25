9fed2f3b3d01c1266aba6bf564ae51396a71a9a5
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-11-25 09:42:36 +0100

    mf

--- src/main/bash/new-document-representation-spark.sh
+++ src/main/bash/new-document-representation-spark.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
 PARALLELISM=500
-export HADOOP_CONF_DIR=/home/kibi9872/web-search-cikm2020-resource-paper-code/hadoop-conf
-export SPARK_HOME=/home/kibi9872/web-search-cikm2020-resource-paper-code/local-spark
 
-/home/kibi9872/web-search-cikm2020-resource-paper-code/local-spark/bin/spark-submit \
+./src/main/bash/run-spark-in-docker.sh spark-submit \
         --conf "spark.speculation=true" \
         --conf "spark.speculation.interval=5000ms" \
         --conf "spark.speculation.multiplier=5" \
@@ -15,4 +13,5 @@ export SPARK_HOME=/home/kibi9872/web-search-cikm2020-resource-paper-code/local-s
         --conf spark.default.parallelism=${PARALLELISM}\
         --num-executors ${PARALLELISM}\
         --driver-memory 15G\
-        target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar ${@}
+        /target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar ${@}
+
