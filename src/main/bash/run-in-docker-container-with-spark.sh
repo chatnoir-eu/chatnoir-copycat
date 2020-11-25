8abe7c8819b183478a49f3ccfe22b532062b2eff
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-11-25 07:52:28 +0100

    Add configuration to start job from docker

---
+++ src/main/bash/run-in-docker-container-with-spark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+if [[ ! -f conf/hadoop/core-site.xml ]]
+then
+    echo "The configuration conf/hadoop/core-site.xml is missing. This file can not be stored in git, because it contains credentials for accessing S3. You can find an example core-site.xml in https://git.webis.de/code-admin/jupyterhub-helm/-/blob/master/conf/hadoop/core-site.xml"
+    exit 1
+fi
+
+docker run --rm -ti \
+	-v ${PWD}/target:/target \
+	-v ${PWD}/conf/hadoop:/etc/hadoop \
+	-v ${PWD}/conf/spark:/etc/spark \
+	-e HADOOP_USER_NAME=${USER} \
+	-e HADOOP_CONF_DIR=/etc/hadoop \
+	-e SPARK_CONF_DIR=/etc/spark \
+	webis/sparknotebook ${@}
+
