83397813801ee1630db68e5e146adff0150ee084
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2021-02-02 12:13:22 +0100

    Add version0 for argsme / notebooks

--- src/main/bash/copy-cat.sh
+++ src/main/bash/copy-cat.sh
@@ -3,7 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
-java -cp ${DIR}/../../../target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar \
-	de.webis.cikm20_duplicates.app.DeduplicateTrecRunFile \
+java -jar ${DIR}/../../../copycat-cli/target/copycat-cli-1.0-SNAPSHOT-jar-with-dependencies.jar \
 	${@}
 
