7821e88ef56cc0ed3514663e3eb6cfa87c51d523
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2021-02-17 20:55:02 +0100

    document cw09-cw12-cc15 dedup task

--- src/main/bash/repartition.sh
+++ src/main/bash/repartition.sh
@@ -2,7 +2,7 @@
 
 PARALLELISM=100
 
-spark-submit \
+./src/main/bash/run-in-docker-container-with-spark.sh spark-submit \
 	--conf "spark.speculation=true" \
 	--conf "spark.speculation.interval=5000ms" \
 	--conf "spark.speculation.multiplier=5" \
@@ -12,5 +12,5 @@ spark-submit \
 	--conf spark.default.parallelism=${PARALLELISM}\
 	--num-executors ${PARALLELISM}\
 	--driver-memory 40G\
-	target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar ${@}
+	/target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar ${@}
 
