a0648d9f4d8f33f23f4bb993357ab3de2fb9902a
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2021-02-18 22:34:00 +0100

    Prepare evaluation on docs-to-remove

--- src/main/bash/ids-to-remove.sh
+++ src/main/bash/ids-to-remove.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -e
 
-PARALLELISM=500
+PARALLELISM=50
 
 ./src/main/bash/run-in-docker-container-with-spark.sh spark-submit \
         --conf "spark.speculation=true" \
         --conf "spark.speculation.interval=5000ms" \
         --conf "spark.speculation.multiplier=5" \
         --conf "spark.speculation.quantile=0.90" \
-        --conf "spark.dynamicAllocation.maxExecutors=1500" \
+	--conf "spark.yarn.maxAppAttempts=1" \
+        --conf "spark.dynamicAllocation.maxExecutors=100" \
         --deploy-mode cluster \
         --class de.webis.cikm20_duplicates.app.CreateIdsToRemove \
         --conf spark.default.parallelism=${PARALLELISM}\
         --num-executors ${PARALLELISM}\
-        --driver-memory 15G\
+        --driver-memory 25G\
+        --executor-memory 15G\
         /target/cikm20-duplicates-1.0-SNAPSHOT-jar-with-dependencies.jar ${@}
 
